230023d7c9d42c013b2ec0ccc53af0394de67630
Jacob Kofod <jacob_kofod@yahoo.com>
2023-12-10 09:02:34 +0100

    Initial code

---
+++ S3JsonArraySplit/src/S3JsonArraySplit/SplitRequest.cs
@@ -0,0 +1,12 @@
+namespace S3JsonArraySplit;
+
+public class SplitRequest
+{
+    public string Region { get; set; }
+    public string Bucketname { get; set; }
+    public string Source { get; set; }
+    public string DestinationPathTemplate { get; set; }
+    public string Fieldname { get; set; }
+    public bool AddContext { get; set; }
+    public string DefaultCategory { get; set; }
+}
---
+++ S3JsonArraySplit/src/S3JsonArraySplit/Splitter.cs
@@ -0,0 +1,112 @@
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace S3JsonArraySplit;
+
+public class Splitter
+{
+    public void Run(SplitRequest Request)
+    {
+        string sourceBucketName = Request.Bucketname;
+        string sourceKey = Request.Source;
+        string destinationBucketName = Request.Bucketname;
+        string splitterField = Request.Fieldname;
+        string region = Request.Region;
+        string defaultCategory = "unassigned";
+        bool addContext = Request.AddContext;
+
+        string sourceJson = ReadS3ObjectAsync(region, sourceBucketName, sourceKey).Result;
+
+        SplitJsonByElementAndUpload(region, sourceJson, splitterField,
+            defaultCategory, destinationBucketName, addContext);
+    }
+
+    static async Task<string> ReadS3ObjectAsync(string Region, string bucketName, string key)
+    {
+        var region = RegionEndpoint.GetBySystemName(Region);
+
+        using (var client = new AmazonS3Client(region)) // Change the region if needed
+        {
+            var request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = key
+            };
+
+            using (GetObjectResponse response = await client.GetObjectAsync(request))
+            using (Stream responseStream = response.ResponseStream)
+            using (StreamReader reader = new StreamReader(responseStream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+
+    static void SplitJsonByElementAndUpload(string Region, string json, 
+        string elementToSplitBy, string defaultCategory, 
+            string destinationBucketName, bool AddContext)
+    {
+        var region = RegionEndpoint.GetBySystemName(Region);
+
+        JArray jsonArray = JArray.Parse(json);
+        Dictionary<string, JArray> splitDocuments = new Dictionary<string, JArray>();
+
+        foreach (JObject item in jsonArray)
+        {
+            string elementValue = item[elementToSplitBy]?.ToString() ?? defaultCategory;
+
+            if (!splitDocuments.ContainsKey(elementValue))
+            {
+                splitDocuments[elementValue] = new JArray();
+            }
+
+            splitDocuments[elementValue].Add(item);
+        }
+
+        using (var client = new AmazonS3Client(region)) // Change the region if needed
+        {
+            foreach (var kvp in splitDocuments)
+            {
+                string categoryName = kvp.Key;
+                string fileName = $"split_{categoryName}.json";
+
+                string jsonContent = null;
+
+                if (AddContext)
+                {
+                    // Create metadata object
+                    JObject metadata = new JObject();
+                    metadata["category"] = categoryName; // Adding category value to metadata
+
+                    // Create JSON object with metadata and data
+                    JObject result = new JObject();
+                    result["metadata"] = metadata;
+                    result["data"] = kvp.Value;
+
+                    jsonContent = result.ToString();
+                }
+                else
+                {
+                    jsonContent = kvp.Value.ToString();
+                }
+
+                // Upload split JSON files to S3
+                var uploadRequest = new PutObjectRequest
+                {
+                    BucketName = destinationBucketName,
+                    Key = fileName,
+                    ContentBody = jsonContent
+                };
+
+                client.PutObjectAsync(uploadRequest).Wait(); // Using Wait for simplicity; consider using async/await in production
+                Console.WriteLine($"Split JSON for '{elementToSplitBy}'='{categoryName}' uploaded to '{fileName}'");
+            }
+        }
+    }
+}
